81e9b4e566cc51206a03fb68a377c201670585f5
Pallavi C <info@joindevops.com>
2025-03-24 17:17:25 +0530

    blue-green

---
+++ 50-acm/variables.tf
@@ -0,0 +1,29 @@
+variable "project_name" {
+   default = "expense"
+}
+
+variable "environment" {
+   default = "dev"
+}
+
+
+
+variable "common_tags" {
+    default = {
+        project = "expense"
+        environment = "dev"
+        terraform = "true"
+    }
+}
+
+variable "zone_id" {
+   default = "Z02887034V5M0478U7X"
+}
+
+variable "domain_name" {
+   default = "rushika.site"
+}
+
+# variable "zone_name" {
+#    default = "rushika.site"
+# }
